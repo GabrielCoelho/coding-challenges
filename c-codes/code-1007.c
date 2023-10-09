6a84424d42f6bb6222b3d47d2071682fc3f3482d
Gabriel Coelho Soares <gbcoe@outlook.com.br>
2023-10-09 13:50:08 -0300

    feat(1007): Input & Output Files

--- c-codes/code-1007.c
+++ c-codes/code-1007.c
@@ -5,18 +5,12 @@
 int main(int argc, char *argv[])
 {
   int A, B, C, D, DIFERENCA;
-  scanf("%d", &A);
-  scanf("%d", &B);
-  scanf("%d", &C);
-  scanf("%d", &D);
-
-  DIFERENCA = ((A*B)-(C*D));
-
-  printf("DIFERENCA = %d\n", DIFERENCA);
-
-
-
-
-
+  FILE *fro = fopen("./input/1007.in", "r");
+  FILE *fwr = fopen("./output/1007.out", "w+");
+  while(fscanf(fro, "%d %d %d %d", &A, &B, &C, &D)!=EOF)
+  {
+    DIFERENCA = ((A*B)-(C*D));
+    fprintf(fwr, "DIFERENCA = %d\n", DIFERENCA);
+  }
   return EXIT_SUCCESS;
 }
