8a3f2ff02e49ebdd8e222c492ab3d3ee178d1485
Gabriel Coelho Soares <gbcoe@outlook.com.br>
2023-10-09 12:49:11 -0300

    Learned how to read and write a file
    Created an input and output folder, and learned how to use the program
    to read and write the files.

--- c-codes/code-1001.c
+++ c-codes/code-1001.c
@@ -6,15 +6,25 @@ int main(int argc, char *argv[])
 {
   int A, B, X;
 
+  FILE *fROM;
+  FILE *fWRT;
+  fWRT = fopen("./output/1001.out", "w+");
+  fROM = fopen("./input/1001.in", "r");
+  while(fscanf(fROM, "%d %d", &A, &B) != EOF){
+    X = A + B;
+    fprintf(fWRT, "%d \n", X);
+  }
+  
+  fclose(fROM);
+  fclose(fWRT);
+
+
+  /*
   printf("Informe o valor de A: ");
   scanf("%d", &A);
 
   printf("Informe o valor de B: ");
   scanf("%d", &B);
-
-  X = A + B;
-
-  printf("X = %d\n", X);
-
+*/
   return EXIT_SUCCESS;
 }
