e68b44ba30dc60209515eea8ce2cd6565c5105ee
Gabriel Coelho Soares <gbcoe@outlook.com.br>
2023-10-09 13:44:59 -0300

    feat(1006): Input & Output Files

--- c-codes/code-1006.c
+++ c-codes/code-1006.c
@@ -5,12 +5,11 @@
 int main(int argc, char *argv[])
 {
   double A, B, C, MEDIA;
-  scanf("%lf", &A);
-  scanf("%lf", &B);
-  scanf("%lf", &C);
-
-  MEDIA = ((A*2)+(B*3)+(C*5))/10;
-
-  printf("MEDIA = %.1lf\n", MEDIA);
+  FILE *fro = fopen("./input/1006.in", "r");
+  FILE *fwr = fopen("./output/1006.out", "w+");
+  while (fscanf(fro, "%lf %lf %lf", &A, &B, &C)!=EOF) {
+    MEDIA = ((A*2) + (B*3) + (C*5))/10;
+    fprintf(fwr, "MEDIA = %.1lf\n", MEDIA);
+  }
   return EXIT_SUCCESS;
 }
